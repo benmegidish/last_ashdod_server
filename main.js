2c4e05a440efd4bac30598aa5f0f828b14e9197a
Ben Megidish <megidish46@gmail.com>
2025-10-21 20:21:19 +0300

    SFGDFG

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+app.get("/", (req, res) => {
+    res.send("Welcome to the API");
+});
 app.use("/api/users", usersRouter);
-
 
 module.exports = app;
